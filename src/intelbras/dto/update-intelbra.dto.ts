2e07affd4f658bcc2c29faf3bb908f4e8fecf9f4
felipebarros <felipe.barros@bepass.com.br>
2025-06-05 18:07:34 -0300

    upload files

--- src/intelbras/dto/update-intelbra.dto.ts
+++
@@ -1,4 +0,0 @@
-import { PartialType } from '@nestjs/mapped-types';
-import { CreateIntelbraDto } from './create-intelbra.dto';
-
-export class UpdateIntelbraDto extends PartialType(CreateIntelbraDto) {}
